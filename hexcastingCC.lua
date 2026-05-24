5c8256e5c84648892f949c1c98911fa65dbfde6c
oliver-error <oliverliamandersen2011@gmail.com>
2026-05-23 17:57:21 -0700

    added the basics to hexcastingCC

--- hexcastingCC.lua
+++ hexcastingCC.lua
@@ -3,12 +3,28 @@ local YCordSlot = 2
 local ZCordSlot = 3
 local staffSide = "left"
 local scrollSide = nil
+local staff = peripheral.wrap(staffSide)
 local spells = {
     overgrow = "NORTH_EAST wqaqwawqaqw",
-    VectorExaltation = "EAST eqqqqq",
+    numToVector = "EAST eqqqqq",
     writeToScroll = "EAST deeeee",
-    readScroll = "EAST aqqqqq"
+    readScroll = "EAST aqqqqq",
+    executeScroll = "SOUTH_EAST deaqq",
+    posToPlayer = "SOUTH_EAST qqqqqdaqaawe",
+    explosion = "EAST aawaawaa",
+    impulse = "SOUTH_WEST awqqqwaqw",
+    ten = " SOUTH_EAST aqaae",
+    zero = "SOUTH_EAST aqaa"
 }
+
+local function getpos()
+    for i = 1, 3 do
+        turtle.select(i)
+        staff.runPattern(spells.readScroll)
+        staff.runPattern(spells.executeScroll)
+    end
+    staff.runPattern(spells.numToVector)
+end
 
 if staffSide == "left" then
     scrollSide = "right"
@@ -16,3 +32,10 @@ else
     scrollSide = "left"
 end
 
+getpos()
+staff.runPattern(spells.posToPlayer)
+staff.runPattern(spells.zero)
+staff.runPattern(spells.ten)
+staff.runPattern(spells.zero)
+staff.runPattern(spells.numToVector)
+staff.runPattern(spells.impulse)
