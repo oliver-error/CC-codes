df453eb203a144d469b0cb9993190fb9b0f78336
oliver-error <oliverliamandersen2011@gmail.com>
2026-05-23 17:09:48 -0700

    added hexcastingCC.lua

---
+++ hexcastingCC.lua
@@ -0,0 +1,18 @@
+local XCordSlot = 1
+local YCordSlot = 2
+local ZCordSlot = 3
+local staffSide = "left"
+local scrollSide = nil
+local spells = {
+    overgrow = "NORTH_EAST wqaqwawqaqw",
+    VectorExaltation = "EAST eqqqqq",
+    writeToScroll = "EAST deeeee",
+    readScroll = "EAST aqqqqq"
+}
+
+if staffSide == "left" then
+    scrollSide = "right"
+else
+    scrollSide = "left"
+end
+
