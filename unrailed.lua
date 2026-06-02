b7a451231954972ef114f5b7988c309d33bd486e
oliver-error <oliverliamandersen2011@gmail.com>
2026-06-01 21:07:51 -0700

    the beginning of quantum wave collapse

--- unrailed.lua
+++ unrailed.lua
@@ -3,12 +3,24 @@ Xoffset = -5
 Yoffset = 52
 Zoffset = 8
 Alternate = true
+local forestTile = false
+local stoneTile = false
 BlockSets = {
     grass = {"moss_block", "grass_block"},
     stone = {"terracotta", "brown_terracotta"},
     forest = {"coarse_dirt", "podzol"},
     water = {"water", "water"},
+    empty = {"air", "air"}
 }
+Map = {}
+local seeds = {"stone", "forest"}
+
+local function tableShuffle(t)
+    for i = #t, 2, -1 do
+        local j = math.random(i)
+        t[i], t[j] = t[j], t[i] -- Swap elements
+    end
+end
 
 local function placeBlock(x, y, z, block)
     commands.exec("setblock " .. (Xoffset + x) .. " " .. (Yoffset + y) .. " " .. (Zoffset + z) .. " " .. block)
@@ -27,15 +39,26 @@ local function makeTile(x, y, z, tileType)
     end
 end
 
-for x = 1, 7 do
-    for y = 1, 7 do
-        local random = math.random(1, 3)
-        if random == 1 then
-            makeTile(x*3, 0, y*3, "stone")
-        elseif random == 2  then
-            makeTile(x*3, 0, y*3, "grass")
-        else
-            makeTile(x*3, 0, y*3, "forest")
+for x = 0, 8 do
+    for y = 0, 8 do
+        if not (x < 9 and y < 3) then
+            table.insert(seeds, "empty")
         end
     end
+end
+
+tableShuffle(seeds)
+
+for x = 0, 8 do
+    for y = 0, 8 do
+        if x < 9 and y < 3 then
+            table.insert(Map, {"grass", x, y})
+        else
+            table.insert(Map, {seeds[x + y], x, y})
+        end
+    end
+end
+
+for index, value in ipairs(Map) do
+    makeTile(value[2]*3, 0, value[3]*3, value[1])
 end
